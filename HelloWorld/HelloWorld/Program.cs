6e67ab4dc5653374b67a11670548646d4ffef219
towplus <towplus79@gmail.com>
2018-10-15 14:13:02 +0900

    first c#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello C#~" + args[0]);
+            Console.WriteLine("Hello C#~" + args[0]);
+        }
+    }
+}
