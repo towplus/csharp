319da563336afce38cf18fdd5c630d0a65fe6fe1
towplus <towplus79@gmail.com>
2018-10-15 15:28:54 +0900

    twice

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 10;
+            int b = a;
+            int c = 50000;
+            try
+            {
+                short d = checked((short)c);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("예외", e.StackTrace);
+            }
+            
+        }
+    }
+}
---
+++ HelloWorld/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyConstants
+    {
+
+        public const double PI = 3.14;
+        public const int MYAGE = 22;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2;
+            double area = MyConstants.PI * (radius * radius);
+            Console.WriteLine("Area = {0}, Age={1}", area, MyConstants.MYAGE);
+            const string name = "홍길동";
+            Console.WriteLine("name : " + name);
+        }
+    }
+}
+
+
