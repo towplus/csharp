b2a1fff9c6c85687d34f55251af37636cd6df257
yang su <towplus79@gmail.com>
2018-10-30 01:05:24 +0900

    10/30/01

---
+++ HomeWork/INotifyTest/Person.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+
+
+
+namespace INotifyTest
+{
+
+
+
+    public class Person : INotifyPropertyChanged
+    {
+
+
+
+        private string name;
+
+
+
+
+
+        // Declare the event 
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+ 
+
+        public Person() { }
+
+
+
+
+
+        public Person(string value) { this.name = value; }
+
+
+
+        //아래 PersonName 속성 값이 변경되면
+
+
+
+        public string PersonName
+
+
+
+        {
+
+
+
+            get { return name; }
+
+
+
+            set
+
+
+
+            {
+
+
+
+                name = value;
+
+
+
+                // Call OnPropertyChanged whenever the property is updated 
+
+
+
+                OnPropertyChanged("PersonName");
+
+
+            }
+
+
+
+        }
+
+
+
+        // Create the OnPropertyChanged method to raise the event 
+
+
+
+        protected void OnPropertyChanged(string name)
+
+
+
+        {
+
+
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
---
+++ HomeWork/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            //CarDependency" 라는 리소스 검색
+            CarDependencyProperty dpSample = TryFindResource("CarDependency") as CarDependencyProperty;
+            MessageBox.Show(dpSample.MyCar);
+        }
+    }
+
+    public class CarDependencyProperty : DependencyObject
+    {
+        //Register Dependency Property  
+        public static readonly DependencyProperty CarDependency = DependencyProperty.Register("MyProperty", typeof(string), typeof(CarDependencyProperty));
+        public string MyCar
+        {
+            get
+            {
+                return (string)GetValue(CarDependency);
+            }
+            set
+            {
+                SetValue(CarDependency, value);
+            }
+        }
+    }
+
+}
